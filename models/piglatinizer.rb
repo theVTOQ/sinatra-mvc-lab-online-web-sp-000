5d7e3f15655d49cb8ee8ccc3ce08aa965d1375a4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-25 03:20:21 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,35 @@
+class PigLatinizer
+  @@vowels = ["a", "e", "i", "o", "u"]
+
+  def initialize(user_phrase)
+    @text = user_phrase.downcase
+  end
+
+  def translation
+    individual_translations = []
+
+
+    @text.split(" ").each do |word|
+      new_word = word
+      first_letter = word[0]
+
+      if @@vowels.include?(first_letter)
+        new_word[0] = ''
+        new_word << "#{first_letter}way"
+      else
+        last_initial_consonant = find_last_initial_consonant(word)
+        new_word.gsub(last_initial_consonant, )
+      end
+    end
+  end
+
+  def find_last_initial_consonant(word)
+    prefix = ""
+    index = 0
+    until @@vowels.include?(word[index])
+      prefix << word[index]
+      index += 1
+    end
+    word[index]
+  end
+end
